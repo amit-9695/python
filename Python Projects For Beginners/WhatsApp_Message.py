9e54e1fea22a86848205acc151b1af6cfdcdbe5c
AMIT SHUKLA <amit800blp@gmail.com>
2024-04-27 21:30:11 +0530

    Update sleep time and message in WhatsApp_Message.pyne

--- Python Projects For Beginners/WhatsApp_Message.py
+++ Python Projects For Beginners/WhatsApp_Message.py
@@ -1,8 +1,8 @@
 import pyautogui
 import time
-time.sleep(5)
+time.sleep(1)
 count=0
-while count<=84:
-    pyautogui.typewrite("Hii Neeraj")
+while count<=100:
+    pyautogui.typewrite("Hii, I am a bot. This is a test message.") # Message you want to send on WhatsAp
     pyautogui.press("enter")
     count=count+1
