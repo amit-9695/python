9c4a3f6077caebf9745913e19ab8b78a35bca675
AMIT SHUKLA <amit800blp@gmail.com>
2024-04-28 23:51:36 +0530

    Add BMI calculation in Exe_2.py

--- Exercise/Exe_2.py
+++ Exercise/Exe_2.py
@@ -3,4 +3,5 @@
 # Here Weight will be in kilograms & Hight will be in meter
 weight=float(input("Enter The Weight In kg.:- "))
 height=float(input("Enter The Height In meter.:- "))
+print("The BMI of the person is:-")
 print(weight/(height**2))
